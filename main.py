9162ddb5110ef54ee48bff5f032d7899acd57ac2
marufjonraxmonov3721-star <marufjonraxmonov3721@gmail.com>
2026-01-18 00:56:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import telebot
 import os
 from flask import Flask, request
 
-# Sizning aniq ma'lumotlaringiz
+# Sizning tokeningiz
 TOKEN = '7838831366:AAFv5D3UR4s9QdoDTLeRTD87r_d8sTAZnIs'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Salom! Men nihoyat ishladim! 😊\nMenga PDF yoki Word fayl yuboring, rasmga aylantirishni o'rganyapman.")
+    bot.reply_to(message, "Salom! Men nihoyat ishladim! 😊\nEndi menga fayl yuborsangiz bo'ladi.")
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
@@ -20,7 +20,7 @@ def getMessage():
 
 @server.route("/")
 def webhook():
-    return "Bot serveri faol!", 200
+    return "Bot serveri ishlamoqda!", 200
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
