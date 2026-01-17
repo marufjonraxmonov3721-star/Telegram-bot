ddb4171cabe23c20b9a8395eb5ac7991a73e0db3
marufjonraxmonov3721-star <marufjonraxmonov3721@gmail.com>
2026-01-18 00:33:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import telebot
 import os
 from flask import Flask, request
 
-# Sizning tokeningiz joylashtirildi
+# Tokeningiz
 TOKEN = '7838831366:AAFv5D3UR4s9QdoDTLeRTD87r_d8sTAZnIs'
+# Render havolangiz (oxirida / belgisiz)
+APP_URL = 'https://telegram-bot-97v7.onrender.com'
+
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Salom! Men Render hostingida ishlayapman. Menga rasmga aylantirish uchun PDF yoki Docx fayl yuboring!")
+    bot.reply_to(message, "Salom! Mana endi men aniq ishlayapman! 😊\nMenga PDF yoki Word fayl yuboring.")
 
 @bot.message_handler(content_types=['document'])
 def handle_docs(message):
-    bot.reply_to(message, f"Rahmat, {message.from_user.first_name}! Faylingizni qabul qildim. Hozircha men test rejimidaman, tez orada rasmga aylantirishni ham o'rganaman.")
+    bot.reply_to(message, "Faylni oldim! Tez orada uni rasmga aylantirishni o'rganib olaman.")
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
@@ -25,8 +28,8 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    # Keyingi qadamda bu yerga Render havolasini ulaymiz
-    return "Bot faol!", 200
+    bot.set_webhook(url=APP_URL + '/' + TOKEN)
+    return "Bot ishlashga tayyor!", 200
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
